be260a14b2c08d6d93be6fdce375b4d03d678f44
Nehal Nevle(EIC) <33775493+Nehal19@users.noreply.github.com>
2019-08-22 16:35:50 +0530

    Update rusted email.rs
    Added output

--- rusted email.rs
+++ rusted email.rs
@@ -11,6 +11,10 @@ fn main() {
     let mut b1:String=io::stdin().read_line(&mut line).unwrap().to_string();
     let sa: Regex = Regex::new(r"^[A-Za-z0-9!#$%&'*+=?^_`{|}~.]+@[a-zA-z0-9]+\.[a-z\.]*").unwrap();
     let w=sa.is_match( &mut line);
-    print!(
-        "{}",w)
-}
+    if w{
+        println!("valid")
+        }
+    else{
+        
+        println!("invali:)
+        }
