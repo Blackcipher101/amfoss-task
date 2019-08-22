b3e005543285f6fa76d709ab1c68759f4e1fcc81
Nehal Nevle(EIC) <33775493+Nehal19@users.noreply.github.com>
2019-08-22 16:10:34 +0530

    Update rusted email.rs
    added * at the end of expression

--- rusted email.rs
+++ rusted email.rs
@@ -6,12 +6,11 @@ fn main() {
     let mut line = String::new();
 
 
+    
     println!("Enter your email id :");
     let mut b1:String=io::stdin().read_line(&mut line).unwrap().to_string();
-    let sa: Regex = Regex::new(\w\w\w\w\w\w\w+@\w\w\w\w+\.\w\w\w).unwrap();
+    let sa: Regex = Regex::new(r"^[A-Za-z0-9!#$%&'*+=?^_`{|}~.]+@[a-zA-z0-9]+\.[a-z\.]*").unwrap();
     let w=sa.is_match( &mut line);
     print!(
-        "{}",
-        w
-    );
+        "{}",w)
 }
