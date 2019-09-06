53554b68c7ad98eb5a750f593eff96c219b52532
Nehal Nevle(EIC) <33775493+Nehal19@users.noreply.github.com>
2019-09-06 15:40:48 +0530

    Update rusted email.rs

--- rusted email.rs
+++ rusted email.rs
@@ -9,7 +9,7 @@ fn main() {
     
     println!("Enter your email id :");
     let mut b1:String=io::stdin().read_line(&mut line).unwrap().to_string();
-    let sa: Regex = Regex::new(r"^[A-Za-z0-9!#$%&'*+=?^_`{|}~.]+@[a-zA-z0-9]+\.[a-z\.]*").unwrap();
+    let sa: Regex = Regex::new(r"^[A-Za-z0-9!#$%&'*+=?^_`{|}~.]+@[a-zA-z0-9]+\.[a-z]*").unwrap();
     let w=sa.is_match( &mut line);
     if w{
         println!("valid")
@@ -18,3 +18,4 @@ fn main() {
         
         println!("invalid")
         }
+}
